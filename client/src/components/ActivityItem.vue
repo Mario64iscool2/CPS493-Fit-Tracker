3e07ef19ca17102147ee873bc0e15c32e87c886e
Cameron Jarvis <mario64iscool2@gmail.com>
2024-03-05 16:35:52 -0500

    Created Components + ActivityItem (Displays a workout on one's feed) + NavBar (Top-level navigation)

---
+++ client/src/components/ActivityItem.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+    user: string,
+    usericon: string,
+    handle: string,
+    msg: string,
+    time: Date,
+    dist: number,
+    duration: number
+}>()
+</script>
+<template>
+    <div class="card is-center">
+        <div class="card-header">
+            <div class="card-header-title"> {{ user }}</div>
+            <div class="card-header-title">@{{ handle }}</div>
+            <div class="card-header-title">{{ time }}</div>
+        </div>
+        <div class="card-content">
+            <div class="content"> {{ msg }}</div>
+            <div class="columns">
+                <div class="column is-half">
+                    <div>
+                        <p class="title">{{ dist }}</p>
+                        <p class="subtitle">Distance</p>
+                    </div>
+                </div>
+                <div class="column is-half has-text-centered">
+                    <div>
+                        <p class="title">{{ duration }}</p>
+                        <p class="subtitle">Duration</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer">
+            
+        </div>
+    </div>
+  </template>
---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav class="navbar">
+        <div class="navbar-brand">
+            <p style="text-align: center;padding:8px 16px">Icon<br/>Pending</p>
+        </div>
+        <span class="navbar-start">
+            <RouterLink class="navbar-item" to="/home">Home</RouterLink>
+            <RouterLink class="navbar-item" to="/profile">My Profile</RouterLink>
+        </span>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
