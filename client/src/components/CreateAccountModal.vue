b989c080305ad3371f8abb557eb3721f1e5cb13f
Mario64iscool2 <cameron@clcpro.com>
2024-03-06 00:17:00 -0500

    Added Modals
    + Sign-In Modal (Non-Functional as of yet.)
    + Account Creation Modal (Also Non-Functional.)

---
+++ client/src/components/CreateAccountModal.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+</script>
+
+<template>
+    <div class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">Create your account</p>
+            </header>
+            <section class="modal-card-body">
+                <p class="subtitle">First things first.</p>
+                        <div class="field is-horizontal">
+                            <div class="field-label is-normal">
+                                <label class="label">Name</label>
+                            </div>
+                            <div class="field-body">
+                                <div class="field">
+                                    <div class="control">
+                                <input class="input" type="text" placeholder="First Name">
+                            </div>
+                                </div>
+                                <div class="field">
+                                    <div class="control">
+                                <input class="input" type="text" placeholder="Last Name">
+                            </div>
+                                </div>
+                            </div>                            
+                        </div>
+                        <div class="field has-addons">
+                            <label class="label">Username</label>
+                            <div class="control">
+                                <input class="input" type="text" placeholder="username" maxlength="16">
+                            </div>
+                        </div>
+                        <div class="field is-narrow">
+
+                                <label class="label">Password</label>
+                                <div class="control">
+                                <input class="input" type="password" placeholder="" maxlength="32">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label class="label">Confirm Password</label>
+                            <div class="control">
+                                <input class="input" type="password" placeholder="" maxlength="32">
+                            </div>
+                        </div>
+            </section>
+            <footer class="modal-card-foot">
+                <div class="buttons">
+                    <button class="button is-primary">Create Account</button>
+                </div>
+            </footer>
+        </div>
+    </div>
+</template>
---
+++ client/src/components/SignInModal.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+let isImperial = ref(true)
+
+</script>
+
+<template>
+    <div class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">You are not logged in!</p>
+            </header>
+            <section class="modal-card-body">
+                <p class="subtitle">Let's get you signed in.</p>
+                <div class="columns">
+                    <div class="column is-three-quarters">
+                        <div class="field">
+                            <label class="label">Username</label>
+                            <div class="control">
+                                <input class="input" type="text" placeholder="username" maxlength="16">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label class="label">Password</label>
+                            <div class="control">
+                                <input class="input" type="password" placeholder="" maxlength="32">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <footer class="modal-card-foot">
+                <div class="buttons">
+                    <button class="button is-primary">Sign In</button>
+                </div>
+            </footer>
+        </div>
+    </div>
+</template>
