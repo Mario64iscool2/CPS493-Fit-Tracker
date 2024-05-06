e6382b58d26e2c3905c8840dfdad135973cc41df
Cameron Jarvis <mario64iscool2@gmail.com>
2024-05-06 13:59:28 -0400

    Host Client on Server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config({path:["./.env"]});
 const express = require('express');
+const path = require('path');
 const config = require('config');
 const auth = require('./controllers/auth');
 const users = require('./controllers/users');
@@ -7,10 +8,13 @@ const workouts = require('./controllers/workouts');
 
 /** @typedef {import('../client/src/model/transportTypes').DataEnvelope<void> } ErrorDataEnvelope */
 
+
 const app = express();
 const PORT = config.get('port');
 
 app.use(express.json())
+
+app.use(express.static('client/dist'))
 
 .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,6 +22,7 @@ app.use(express.json())
     res.setHeader('Access-Control-Allow-Headers', '*');
     next();
 })
+
 
 .use('/api/v1/users',users)
 .use('/api/v1/workouts',workouts)
@@ -31,6 +36,10 @@ app.use((err, req, res, next) => {
         message: err.message || "Unable to Complete Request"
     };
     res.status(500).send(results);
+})
+
+app.use((req,res)=> {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
 })
 
 app.listen(PORT, () => {
